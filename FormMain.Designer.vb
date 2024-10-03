c3630328cf6806f48e06f43bed879084b74d1b2f
Ivan Camperchioli <ivanemanuel2137@gmail.com>
2024-10-03 19:18:57 -0300

    initial commit

---
+++ FormMain.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormMain))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.LblWeeklyPrediction = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BtnAstralChart = New System.Windows.Forms.ToolStripLabel()
+        Me.Lbl_text = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.BtnSubscribe = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FlowLayoutPanel3 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel2 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ToolStrip1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.FlowLayoutPanel3.SuspendLayout()
+        Me.FlowLayoutPanel2.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LblWeeklyPrediction, Me.ToolStripSeparator1, Me.BtnAstralChart})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1094, 25)
+        Me.ToolStrip1.TabIndex = 11
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'LblWeeklyPrediction
+        '
+        Me.LblWeeklyPrediction.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblWeeklyPrediction.IsLink = True
+        Me.LblWeeklyPrediction.LinkColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.LblWeeklyPrediction.Margin = New System.Windows.Forms.Padding(20, 1, 20, 2)
+        Me.LblWeeklyPrediction.Name = "LblWeeklyPrediction"
+        Me.LblWeeklyPrediction.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.LblWeeklyPrediction.Size = New System.Drawing.Size(202, 22)
+        Me.LblWeeklyPrediction.Tag = ""
+        Me.LblWeeklyPrediction.Text = "Predicciones semanales"
+        Me.LblWeeklyPrediction.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'BtnAstralChart
+        '
+        Me.BtnAstralChart.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnAstralChart.IsLink = True
+        Me.BtnAstralChart.LinkColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnAstralChart.Margin = New System.Windows.Forms.Padding(20, 1, 0, 2)
+        Me.BtnAstralChart.Name = "BtnAstralChart"
+        Me.BtnAstralChart.Size = New System.Drawing.Size(105, 22)
+        Me.BtnAstralChart.Text = "Carta Astral"
+        '
+        'Lbl_text
+        '
+        Me.Lbl_text.AutoSize = True
+        Me.Lbl_text.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Lbl_text.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Lbl_text.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbl_text.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Lbl_text.Location = New System.Drawing.Point(3, 0)
+        Me.Lbl_text.Name = "Lbl_text"
+        Me.Lbl_text.Size = New System.Drawing.Size(294, 79)
+        Me.Lbl_text.TabIndex = 0
+        Me.Lbl_text.Text = " Astrología Tradicional y Análisis Astrológico"
+        Me.Lbl_text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Lbl_text, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.BtnSubscribe, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBox1, 0, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(70, 88)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.79661!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.20339!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(600, 118)
+        Me.TableLayoutPanel1.TabIndex = 13
+        '
+        'BtnSubscribe
+        '
+        Me.BtnSubscribe.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnSubscribe.Dock = System.Windows.Forms.DockStyle.Left
+        Me.BtnSubscribe.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.BtnSubscribe.Location = New System.Drawing.Point(303, 82)
+        Me.BtnSubscribe.Name = "BtnSubscribe"
+        Me.BtnSubscribe.Size = New System.Drawing.Size(91, 33)
+        Me.BtnSubscribe.TabIndex = 15
+        Me.BtnSubscribe.Text = "Subscribirse"
+        Me.BtnSubscribe.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.TextBox1.Location = New System.Drawing.Point(3, 83)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(294, 32)
+        Me.TextBox1.TabIndex = 14
+        '
+        'FlowLayoutPanel3
+        '
+        Me.FlowLayoutPanel3.Controls.Add(Me.Label3)
+        Me.FlowLayoutPanel3.Location = New System.Drawing.Point(778, 510)
+        Me.FlowLayoutPanel3.Name = "FlowLayoutPanel3"
+        Me.FlowLayoutPanel3.Size = New System.Drawing.Size(316, 78)
+        Me.FlowLayoutPanel3.TabIndex = 27
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(3, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(310, 68)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = " Los informes contienen información sobre la posibilidad de acontecimientos de la" &
+    "s personas. Con esto podemos tomar medidas de posibles acontecimientos que pueda" &
+    "n ocurrir. "
+        '
+        'FlowLayoutPanel2
+        '
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label2)
+        Me.FlowLayoutPanel2.Location = New System.Drawing.Point(778, 377)
+        Me.FlowLayoutPanel2.Name = "FlowLayoutPanel2"
+        Me.FlowLayoutPanel2.Size = New System.Drawing.Size(316, 127)
+        Me.FlowLayoutPanel2.TabIndex = 26
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(308, 119)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = resources.GetString("Label2.Text")
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label1)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(778, 261)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(316, 110)
+        Me.FlowLayoutPanel1.TabIndex = 24
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(308, 102)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(822, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(173, 26)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "Sobre Astrología"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.predicciones.My.Resources.Resources.astrologia
+        Me.PictureBox4.Location = New System.Drawing.Point(70, 261)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(405, 411)
+        Me.PictureBox4.TabIndex = 33
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.predicciones.My.Resources.Resources.check
+        Me.PictureBox1.Location = New System.Drawing.Point(714, 261)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(61, 66)
+        Me.PictureBox1.TabIndex = 31
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.predicciones.My.Resources.Resources.check
+        Me.PictureBox3.Location = New System.Drawing.Point(714, 510)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(61, 62)
+        Me.PictureBox3.TabIndex = 29
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.predicciones.My.Resources.Resources.check
+        Me.PictureBox2.Location = New System.Drawing.Point(714, 377)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(61, 62)
+        Me.PictureBox2.TabIndex = 28
+        Me.PictureBox2.TabStop = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(75, 209)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(282, 15)
+        Me.Label5.TabIndex = 34
+        Me.Label5.Text = "Subscríbase para recibir notificaciones semanales"
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(986, 486)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.FlowLayoutPanel3)
+        Me.Controls.Add(Me.FlowLayoutPanel2)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Name = "FormMain"
+        Me.Text = "Horóscopo"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.FlowLayoutPanel3.ResumeLayout(False)
+        Me.FlowLayoutPanel3.PerformLayout()
+        Me.FlowLayoutPanel2.ResumeLayout(False)
+        Me.FlowLayoutPanel2.PerformLayout()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents LblWeeklyPrediction As ToolStripLabel
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents BtnAstralChart As ToolStripLabel
+    Friend WithEvents Lbl_text As Label
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BtnSubscribe As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents FlowLayoutPanel3 As FlowLayoutPanel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents FlowLayoutPanel2 As FlowLayoutPanel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label5 As Label
+End Class
